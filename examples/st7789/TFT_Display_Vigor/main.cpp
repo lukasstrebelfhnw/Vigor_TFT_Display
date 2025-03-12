6e1b7af3eb5bafb24693cf751113ad941129eb8c
lukasstrebelfhnw <lukas.strebel1@students.fhnw.ch>
2025-03-12 11:03:36 +0100

    ...

--- examples/st7789/TFT_Display_Vigor/main.cpp
+++ examples/st7789/TFT_Display_Vigor/main.cpp
@@ -112,7 +112,7 @@ void runHMIStateMachine(HMIState state)
 	{
 	case HMIState::STARTUP:
 		std::cout << "State: StartUp" << std::endl;
-		myVigorTFT.createInitDisplay(logoVigorWidth, logoVigorHeight, pathLogoVigor, vigorVersion, myTFTHeight, myTFTWidth);
+		myVigorTFT.createInitDisplay(logoVigorWidth, logoVigorHeight, pathLogoVigor, vigorVersion, myTFTWidth, myTFTHeight);
 		break;
 	case HMIState::INIT:
 		std::cout << "State: Init" << std::endl;
