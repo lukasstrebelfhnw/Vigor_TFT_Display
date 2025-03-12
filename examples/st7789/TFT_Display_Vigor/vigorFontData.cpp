3ab27c668ced7f0fe9d915e4ec39406202e0be9f
lukasstrebelfhnw <lukas.strebel1@students.fhnw.ch>
2025-03-12 21:59:59 +0100

    backup

---
+++ examples/st7789/TFT_Display_Vigor/vigorFontData.cpp
@@ -0,0 +1,16 @@
+/* --- vigorFontData.cpp --- */
+
+/* ------------------------------------------
+author: Lukas
+date: 3/12/2025
+------------------------------------------ */
+
+#include "vigorFontData.h"
+
+vigorFontData::vigorFontData() {
+    // Constructor
+}
+
+vigorFontData::~vigorFontData() {
+    // Destructor
+}
---
+++ examples/st7789/TFT_Display_Vigor/vigorFontData.h
@@ -0,0 +1,20 @@
+/* --- vigorFontData.h --- */
+
+/* ------------------------------------------
+Author: Lukas
+Date: 3/12/2025
+------------------------------------------ */
+
+#ifndef VIGORFONTDATA_H
+#define VIGORFONTDATA_H
+
+class vigorFontData {
+public:
+    vigorFontData();
+    ~vigorFontData();
+
+private:
+
+};
+
+#endif // VIGORFONTDATA_H
