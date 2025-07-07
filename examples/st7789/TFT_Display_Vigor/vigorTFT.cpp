a711b96cb4cd3c538a2b4d29d5e21a82117195c8
danielfhnw <daniel.jakober@fhnw.ch>
2025-07-07 15:39:49 +0200

    err

--- examples/st7789/TFT_Display_Vigor/vigorTFT.cpp
+++ examples/st7789/TFT_Display_Vigor/vigorTFT.cpp
@@ -84,12 +84,12 @@ void vigorTFT::createDisplay(
 		if (dataIt != data.end())
 		{
 			// If the data is found, draw the text with the provided value
-			this->drawText(tb_instance, dataIt->second);
+			this->drawText(textBoxDef, dataIt->second);
 		}
 		else
 		{
 			// If the data is not found, draw the text with the default value
-			this->drawText(tb_instance);
+			this->drawText(textBoxDef);
 		}
 	}
 }
